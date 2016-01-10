985479fd81bfb72ff9c3ae746c9accc48fb11b83
Raajay Viswanathan <raajay.v@gmail.com>
2016-01-09 21:33:46 -0500

    change the load script to clean up existing files

--- load_hdfs.sh
+++ load_hdfs.sh
@@ -1,6 +1,7 @@
 #!/bin/bash 
 VERSION=tachyon
-BASE_DIR=/media/code-netopt
+BASE_DIR=${CODE_DIR}
 
+${HADOOP_HOME}/bin/hdfs dfs -rm -r /apps/${VERSION}
 ${HADOOP_HOME}/bin/hdfs dfs -mkdir -p /apps/${VERSION}
 ${HADOOP_HOME}/bin/hdfs dfs -copyFromLocal ${BASE_DIR}/tachyon/clients/client/target/tachyon-client-0.9.0-SNAPSHOT-jar-with-dependencies.jar /apps/${VERSION}
